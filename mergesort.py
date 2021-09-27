f90ee54780d491925d7f8e79a2b97ced65f1e4de
tushar720 <60055667+tushar720@users.noreply.github.com>
2021-09-27 17:13:14 +0530

    Create mergesort.py

---
+++ mergesort.py
@@ -0,0 +1,80 @@
+import time
+
+#merge sort function!
+def merge(arr, l, m, r):
+
+    n1 = m - l + 1
+
+    n2 = r - m
+ 
+
+    # creating another empty arrays
+    L = [0] * (n1)
+
+    R = [0] * (n2)
+ 
+
+    #taking data to arrays L[] and R[]
+
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+        
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+        
+    i = 0     
+    j = 0    
+    k = l    
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+        
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+def mergeSort(arr, l, r):
+    if l < r:
+        m = l+(r-l)//2
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+  
+# main function
+
+#starting time
+   start = time.time()
+arr=[]
+
+n= int(input("enter ur size:"))
+print("enter ur elements:")
+for i in range(0,n):
+	num = int(input())
+	arr.append(num)
+
+print("Given array is")
+
+for i in range(n):
+    print("%d" % arr[i]),
+ 
+mergeSort(arr, 0, n-1)
+print("\n\nSorted array is")
+for i in range(n):
+    print("%d" % arr[i])
+    
+end = time.time()
+
+#total time taken
+print(f"runtime of the program is{end - start}")
